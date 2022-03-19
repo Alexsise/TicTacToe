3bb261c3457b8d37a00c38a23b4cb772876ba755
Al <alexsise.ur@gmail.com>
2022-03-19 13:37:58 +0300

    Create PlayerBehaviour.cs
    Файл Player.cs переименован в PlayerBehaviour.cs

---
+++ PlayerBehaviour.cs
@@ -0,0 +1,34 @@
+namespace TicTacToe;
+using static Field;
+using static Field.Occupation;
+
+public static class PlayerBehaviour
+{
+    public static void PlayerMove(ref Occupation[] gameField)
+    {
+        var playerTurn = true;
+        while (playerTurn)
+        {
+            Console.Write("Choose an unoccupied field: ");
+            int i;
+            try
+            {
+                i = Convert.ToInt16(Console.ReadLine());
+            }
+            catch
+            {
+                continue;
+            }
+
+            if (i is > 10 or < 1)
+                continue;
+            
+            if (gameField[i - 1] != Empty)
+                continue;
+            gameField[i - 1] = Player;
+            playerTurn = false;
+        }
+    }
+    
+    
+}
